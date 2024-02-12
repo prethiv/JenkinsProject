7c035377140f66d9d3cea07806a6bad15ae9647f
prethiv <prethiv191@gmail.com>
2024-02-12 18:15:24 +0530

    test

---
+++ src/test/java/org/example/TempTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TempTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        System.out.println("Nothing to test");
+    }
+}
